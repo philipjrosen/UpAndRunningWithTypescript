9b9da6efd3c9216bea8e807e3a8eb7bc9eae971e
Philip Rosen <philipjrosen@gmail.com>
2017-02-08 12:45:09 -0800

    Using Typings and Loading From node_modules

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,10 @@
 "use strict";
 var _ = require("lodash");
+var Observable_1 = require("rxjs/Observable");
+require("rxjs/add/observable/timer");
+require("rxjs/add/observable/interval");
+Observable_1.Observable.interval(1000)
+    .subscribe(function (x) { return console.log(x); });
 var App = (function () {
     function App() {
         this.title = "Egghead";
