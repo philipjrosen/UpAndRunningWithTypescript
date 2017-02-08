c967f452d3bbf7572f10ec40d54fce250b98e108
Philip Rosen <philipjrosen@gmail.com>
2017-02-08 10:23:32 -0800

    Configuring a Typescript File/Stopping a TypeScript Build

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+require("./two");
+var App = (function () {
+    function App() {
+        this.name = 'John Lennon';
+    }
+    return App;
+}());
---
+++ main.ts
@@ -0,0 +1,5 @@
+import "./two";
+
+class App {
+  name = 'John Lennon';
+}
