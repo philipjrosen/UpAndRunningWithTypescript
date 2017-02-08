22f0849bc0daa95a02f6ed2274ac4fbfb77bc578
Philip Rosen <philipjrosen@gmail.com>
2017-02-07 19:53:37 -0800

    delete files

--- bundle.js
+++
@@ -1,7 +0,0 @@
-var Person = (function () {
-    function Person() {
-        this.name = 'Philip';
-    }
-    return Person;
-}());
-;
--- two.js
+++
@@ -1,5 +0,0 @@
-var Two = (function () {
-    function Two() {
-    }
-    return Two;
-}());
